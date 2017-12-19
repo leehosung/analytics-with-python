1f03d6090e6417b62502693c39402a459fa959de
Ho Sung Lee <lee.ho.sung@gmail.com>
2017-12-19 20:47:04 +0900

    first commit

---
+++ chapter1/first_script.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+print("Output #1: I'm excited to learn Python.")
+
+# 두 수를 더한다.
+x = 4
+y = 5
+z = x + y
+print("Output #2: Four plus fice equals {0:d}.".format(z))
+
+# 두 리스트를 더한다.
+a = [1, 2, 3, 4]
+b = ["first", "second", "third", "fourth"]
+c = a + b
+print("Output #3: {0}, {1}, {2}".format(a, b, c))
